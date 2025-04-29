3cfb80f7210570192aabe10c5880d2254d30e331
Rosmarie Debski <64656859+Glitterrosie@users.noreply.github.com>
2025-04-29 22:36:44 +0200

    add card slots and deck with different cards

---
+++ scenes/card_slot.gd
@@ -0,0 +1,3 @@
+extends Node2D
+
+@export var card_in_slot = false
---
+++ scripts/card_database.gd
@@ -0,0 +1,23 @@
+const ALL_CARDS_META = {
+	"Card0": [1,2],
+	"Card1": [1,2],
+	"Card2": [1,2],
+	"Card3": [1,2],
+	"Card4": [1,2],
+	"Card5": [1,2],
+	"Card6": [1,2],
+	"Card7": [1,2],
+	"Card8": [1,2]
+}
+
+const ALL_CARDS = {
+	"Card0": "res://assets/cards/card1.png",
+	"Card1": "res://assets/cards/card1.png",
+	"Card2": "res://assets/cards/card1.png",
+	"Card3": "res://assets/cards/card1.png",
+	"Card4": "res://assets/cards/card2.png",
+	"Card5": "res://assets/cards/card2.png",
+	"Card6": "res://assets/cards/card2.png",
+	"Card7": "res://assets/cards/card2.png",
+	"Card8": "res://assets/cards/card2.png"
+}
